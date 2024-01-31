8124d75164a0140883335f4105823529693ab4cf
githubscientist <sathish.datascientist@gmail.com>
2024-01-31 20:36:36 +0530

    datatypes --number type

---
+++ day2/datatypes.js
@@ -0,0 +1,18 @@
+/*
+    Variables: a label that points to a memory location or address where we can store data.
+
+    Data Types: What kind/type of data we can store in a variable.
+
+    1. Number
+        - any number, including numbers with decimals: 4, 8, 1516, 23.42
+
+    - To check the type of data we can use typeof function.
+*/
+
+// let age = 25;
+
+// console.log(typeof(age));
+
+let percentage = 99.9;
+
+console.log(typeof(percentage));
