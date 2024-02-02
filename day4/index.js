6ea0e1743905815226c84ed95646c220d835aaa7
githubscientist <sathish.datascientist@gmail.com>
2024-02-02 19:38:38 +0530

    iterating json object using for...in loop

--- day4/index.js
+++ day4/index.js
@@ -1,19 +1,10 @@
-// JSON String
-
 let person = {
     name: "John",
     age: 30,
     city: "New York"
 };
 
-// console.log(JSON.stringify(person)); // {"name":"John","age":30,"city":"New York"}
-
-// console.log(person["name"]);
-
-let JSONString = '{"name":"John","age":30,"city":"New York"}';
-
-// console.log(JSONString.split(',')[0].split('{')[1]);
-
-let JSONObject = JSON.parse(JSONString);
-
-console.log(JSONObject["name"]);
+// iteration using for...in loop
+for (let key in person) {
+    console.log(person[key]);
+}
---
+++ day4/topics.md
@@ -0,0 +1,7 @@
+day3: additional
+
+JSON iteration
+XMLHttpRequest
+Hoisting & scope
+
+Task: https://docs.google.com/document/d/1cNDeT0CKJwqXYDeSIlmJVhx_t4AFvR_0Ypa1cp2rhJk/edit?usp=sharing
