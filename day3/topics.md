f74953bdb60c8ba31fca522cd748757f7c00143a
githubscientist <sathish.datascientist@gmail.com>
2024-02-02 19:16:54 +0530

    json --a simple example

--- day3/topics.md
+++
@@ -1,8 +0,0 @@
-Day 3: JS array & objects
-
-Introduction to Array
-Array and JSON iteration
-XMLHttpRequest
-Hoisting & scope
-
-Task: https://docs.google.com/document/d/1cNDeT0CKJwqXYDeSIlmJVhx_t4AFvR_0Ypa1cp2rhJk/edit?usp=sharing
---
+++ day4/index.js
@@ -0,0 +1,27 @@
+/* 
+
+    JSON: JavaScript Object Notation
+
+    - represented as key-value pairs
+
+*/
+
+// let person = [
+//     'sathish',
+//     35,
+//     true,
+//     ['reading', 'coding', 'travelling'],
+// ];
+
+// console.log(person[3][2]); // gets the person's age
+
+let person = {
+    name: 'sathish',
+    age: 35,
+    isMarried: true,
+    hobbies: ['reading', 'coding', 'travelling'],
+}
+
+console.log(person['hobbies']);
+
+// console.log(person.hobbies[0]);
