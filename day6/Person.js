7ed3e6b0f2c4d86becfd7a0326c671547decdd61
githubscientist <sathish.datascientist@gmail.com>
2024-02-08 19:08:53 +0530

    day 6 task movie doubt

---
+++ day6/Person.js
@@ -0,0 +1,42 @@
+// Courses: CS, IT, ECE
+// Student -> class definition
+class Student {
+    constructor(name, email, course) {
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
+
+    // getStudentsByCourse(students, course) {
+    //     let filteredStudents = [];
+
+    //     students.forEach(student => {
+    //         if (student.course === course) {
+    //             filteredStudents.push(student);
+    //         }
+    //     });
+
+    //     return filteredStudents;
+    // }
+
+    getStudentsByCourse(students, course) {
+        return students.filter(student => student.course === course);
+    }
+}
+
+// create an array of Students
+const students = [];
+
+// add multiple student objects into the students array
+students.push(
+    new Student('sathish', 'sathish@gmail.com', 'CS'),
+    new Student('krish', 'krish@gmail.com', 'CS'),
+    new Student('prakash', 'prakash@gmail.com', 'IT'),
+    new Student('suresh', 'suresh@gmail.com', 'ECE'),
+    new Student('ramesh', 'ramesh@gmail.com', 'ECE'),
+    new Student('john', 'john@gmail.com', 'CS')
+);
+
+let studentB = new Student('admin', 'admin@gmail.com', 'MGT');
+
+console.log(studentB.getStudentsByCourse(students, 'CS'));
