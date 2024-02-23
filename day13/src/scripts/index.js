7cd5360943b7b3a63b042e8c9013698b8b7406fb
githubscientist <sathish.datascientist@gmail.com>
2024-02-23 20:10:33 +0530

    multiple elements createElement --h1 --div --paragraph

--- day13/src/scripts/index.js
+++ day13/src/scripts/index.js
@@ -1,8 +1,18 @@
+// create a div element
+const header = document.createElement('div');
+
 // create a h1 element
 const h1 = document.createElement('h1');
-
-// add some text to the h1 element
 h1.innerText = 'DOM-DEMO';
 
-// append the h1 element to the body
-document.body.appendChild(h1);
+// create a p element
+const paragraph = document.createElement('p');
+paragraph.innerText = 'The Document Object Model (DOM) is a programming interface for web documents. It represents the page so that programs can change the document structure, style, and content. The DOM represents the document as nodes and objects. That way, programming languages can connect to the page.';
+
+// append h1 and paragraph to the div
+header.appendChild(h1);
+header.appendChild(paragraph);
+
+// append the div to the body
+document.body.appendChild(header);
+
