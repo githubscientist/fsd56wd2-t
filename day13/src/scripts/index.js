14ec816f7b5f39a2113616a91c5494c777f27be0
githubscientist <sathish.datascientist@gmail.com>
2024-02-23 19:52:32 +0530

    createElement example

---
+++ day13/src/scripts/index.js
@@ -0,0 +1,8 @@
+// create a h1 element
+const h1 = document.createElement('h1');
+
+// add some text to the h1 element
+h1.innerText = 'DOM-DEMO';
+
+// append the h1 element to the body
+document.body.appendChild(h1);
