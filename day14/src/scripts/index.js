6af33b8f1e7c6e589fb26cc9e582cdea70ca9987
githubscientist <sathish.datascientist@gmail.com>
2024-02-26 20:59:30 +0530

    setTimeout --example alert box

--- day14/src/scripts/index.js
+++ day14/src/scripts/index.js
@@ -1,12 +1,7 @@
-// console.log(window.setTimeout);
+let notification = document.getElementById('notification');
 
-/*
-    setTimeout => a function that takes a callback function and a time in milliseconds, it will call the callback function after the time has passed.
-*/
-
-function sayHello() {
-    console.log('hello');
+let hideNotification = function () {
+    notification.style.display = 'none';
 }
 
-// call the function after 3 seconds from when the page loads
-window.setTimeout(sayHello, 10000);
+setTimeout(hideNotification, 10000);
