654b69ef8f4f3b5d48585e14eaa4359bcd5b2b3b
githubscientist <sathish.datascientist@gmail.com>
2024-02-27 20:17:38 +0530

    event handling --revisited

--- day15/src/scripts/index.js
+++ day15/src/scripts/index.js
@@ -1,7 +1,46 @@
-let registerButton = document.getElementById('registerButton');
+// let registerButton = document.getElementById('registerButton');
+// let registerButton = document.querySelector('#registerButton');
+let registerButton = document.querySelector('.registerButton');
 
-let handleRegisterButtonClick = () => {
-    console.log('Register button clicked');
-}
+// button click event
+// registerButton.addEventListener('click', function () {
+//     console.log('button clicked!');
+// });
 
-registerButton.addEventListener('dblclick', handleRegisterButtonClick);
+// let registerButtonClickHandler = (event) => {
+//     event.stopPropagation();
+//     console.log(event.target);
+//     registerButton.style.backgroundColor = 'red';
+//     registerButton.style.border = 'none';
+//     registerButton.style.color = 'white';
+//     registerButton.style.padding = '10px 20px';
+// }
+
+// // on button click, change the color of the button
+// registerButton.addEventListener('click', registerButtonClickHandler);
+
+// document.body.addEventListener('click', function () {
+//     registerButton.style.backgroundColor = '#EFEFEF';
+//     registerButton.style.color = 'black';
+//     console.log(event.target);
+// });
+
+registerButton.addEventListener('mouseover', () => {
+    registerButton.style.backgroundColor = 'red';
+    registerButton.style.color = 'white';
+});
+
+registerButton.addEventListener('mouseout', function () {
+    registerButton.style.backgroundColor = '#EFEFEF';
+    registerButton.style.color = 'black';
+});
+
+registerButton.addEventListener('click', function () {
+    let colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'brown', 'black', 'grey'];
+    let randomNumber = Math.floor(Math.random()*10);
+    registerButton.style.backgroundColor = colors[randomNumber];
+});
+
+let oldPrice = document.getElementById('oldPrice');
+
+oldPrice.style.textDecoration = 'line-through';
--- readme.md
+++ readme.md
@@ -2,11 +2,13 @@
 
 [] Blog Page - with NAV + FLEX + GRID for entire page layout  
 [] Github Profile Page
+[] Bootstrap Breakpoints  
+[x] Event Handling
 
 ### Day -3: Recap of Topics
 
 **Contents:**
 
 [] recap of DOM topics  
-[] Mandatory session practice task  
+[x] Mandatory session practice task  
 https://github.com/rvsp/HTML-CSS/blob/master/Mini-tasks/003-JS-DOM.md
