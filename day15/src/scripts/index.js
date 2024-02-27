edf2118cc8434d961f8f51e16a5faebd9fe807fa
githubscientist <sathish.datascientist@gmail.com>
2024-02-27 21:49:48 +0530

    twitter char counter

--- day15/src/scripts/index.js
+++ day15/src/scripts/index.js
@@ -1,46 +1,18 @@
-// let registerButton = document.getElementById('registerButton');
-// let registerButton = document.querySelector('#registerButton');
-let registerButton = document.querySelector('.registerButton');
+let tweet = document.getElementById('tweet');
+let charCount = document.getElementById('charCount');
 
-// button click event
-// registerButton.addEventListener('click', function () {
-//     console.log('button clicked!');
-// });
-
-// let registerButtonClickHandler = (event) => {
-//     event.stopPropagation();
-//     console.log(event.target);
-//     registerButton.style.backgroundColor = 'red';
-//     registerButton.style.border = 'none';
-//     registerButton.style.color = 'white';
-//     registerButton.style.padding = '10px 20px';
-// }
-
-// // on button click, change the color of the button
-// registerButton.addEventListener('click', registerButtonClickHandler);
-
-// document.body.addEventListener('click', function () {
-//     registerButton.style.backgroundColor = '#EFEFEF';
-//     registerButton.style.color = 'black';
-//     console.log(event.target);
-// });
-
-registerButton.addEventListener('mouseover', () => {
-    registerButton.style.backgroundColor = 'red';
-    registerButton.style.color = 'white';
+tweet.addEventListener('input', function () {
+    console.log(event.target.value);
+    let message = event.target.value;
+    charCount.textContent = `${message.length}`;
 });
 
-registerButton.addEventListener('mouseout', function () {
-    registerButton.style.backgroundColor = '#EFEFEF';
-    registerButton.style.color = 'black';
+charCount.style.color = 'red';
+
+tweet.addEventListener('focusin', function () {
+    tweet.style.outline = '1px solid blue';
 });
 
-registerButton.addEventListener('click', function () {
-    let colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'brown', 'black', 'grey'];
-    let randomNumber = Math.floor(Math.random()*10);
-    registerButton.style.backgroundColor = colors[randomNumber];
+tweet.addEventListener('focusout', function () {
+    tweet.style.outline = '1px solid #29A6EB';
 });
-
-let oldPrice = document.getElementById('oldPrice');
-
-oldPrice.style.textDecoration = 'line-through';
