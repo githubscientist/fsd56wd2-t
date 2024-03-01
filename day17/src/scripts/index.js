54fb317b04fa273cb7a5835597a28bfa607e6dc9
githubscientist <sathish.datascientist@gmail.com>
2024-03-01 21:10:43 +0530

    fetch and then

--- day17/src/scripts/index.js
+++ day17/src/scripts/index.js
@@ -1,51 +1,80 @@
-// let promise = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         reject();
-//     }, 3000);
-// });
+/*
+    fetch API -> modern way to make HTTP requests rather than using XMLHttpRequest (XHR)
+*/
+let fetchCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then((response) => {
+            return response.json();
+        })
+        .catch(() => {
+            console.log('Data fetching failed');
+        })
+        .then((countries) => {
+            let container = document.querySelector('.container');
 
-// promise
-//     .then(
-//         () => {
-//             console.log('Promise was resolved');
-//         },
+            let table = document.createElement('table');
 
-//         () => {
-//             console.log('Promise was rejected');
-//         }
-//     )
+            table.setAttribute('cellpadding', '10');
+            table.classList.add('table');
 
-// ----------------------------------------------------------
+            let headRow = document.createElement('tr');
 
-let promise = new Promise((resolve, reject) => {
-    let randomNumber = Math.random() * 10;
-    console.log(randomNumber);
-    if (randomNumber > 5) {
-        setTimeout(() => {
-            resolve(randomNumber);
-        }, 3000);
-    } else {
-        setTimeout(() => {
-            reject(randomNumber);
-        }, 3000);
-    }
-});
+            let firstHead = document.createElement('th');
+            firstHead.textContent = 'Name';
 
-// print the promise state initially
-// console.log(promise);
+            let secondHead = document.createElement('th');
+            secondHead.textContent = 'Capital';
 
-promise
-    .then(
-        (result) => {
-            console.log('Promise was resolved with the result: ' + result);
-        },
+            let thirdHead = document.createElement('th');
+            thirdHead.textContent = 'Region';
 
-        (result) => {
-            console.log('Promise was rejected with the result: ' + result);
-        }
-    )
+            let fourthHead = document.createElement('th');
+            fourthHead.textContent = 'Population';
 
-// print the promise state after 3 seconds
-// setTimeout(() => {
-//     console.log(promise);
-// }, 3000);
+            let fifthHead = document.createElement('th');
+            fifthHead.textContent = 'Flag';
+
+            headRow.append(
+                firstHead,
+                secondHead,
+                thirdHead,
+                fourthHead, 
+                fifthHead
+            );
+
+            table.append(headRow);
+
+            countries.forEach(country => {
+                let row = document.createElement('tr');
+
+                let name = document.createElement('td');
+                name.textContent = country.name.common ? country.name.common : 'N/A';
+
+                let capital = document.createElement('td');
+                if (country.capital) {
+                    capital.textContent = country.capital[0];
+                } else {
+                    capital.textContent = 'N/A';
+                }
+
+                let region = document.createElement('td');
+                region.textContent = country.region;
+
+                let population = document.createElement('td');
+                population.textContent = country.population;
+
+                let flag = document.createElement('td');
+                flag.innerHTML = `<img src="${country.flags.png}" alt="${country.name.common}" width="50px" height="50px">`;
+
+                row.append(
+                    name, capital, region, population, flag
+                )
+                
+                table.appendChild(row);
+
+            })
+            container.append(table);
+        })
+};
+
+fetchCountries();
